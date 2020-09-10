13a189a1c0b4b7f76ad7980566daaf29a6e04050
Andrey <KonkeyDong37@gmail.com>
2020-09-10 20:04:43 +0500

    Add new project

---
+++ CoreData Entities/User+CoreDataClass.swift
@@ -0,0 +1,16 @@
+//
+//  User+CoreDataClass.swift
+//  MealTime
+//
+//  Created by Андрей on 10.09.2020.
+//  Copyright © 2020 Ivan Akulov. All rights reserved.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(User)
+public class User: NSManagedObject {
+
+}
